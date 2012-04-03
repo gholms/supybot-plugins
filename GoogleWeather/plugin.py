ce902b2f52d1d7046bc5fcd43b34c895ba33e35a
Garrett Holmstrom <gholms@fedoraproject.org>
2012-04-03 10:14:14 -0700

    Deal with missing weather XML elements properly

--- GoogleWeather/plugin.py
+++ GoogleWeather/plugin.py
@@ -49,11 +49,39 @@ class GoogleWeather(callbacks.Plugin):
             url = ('http://www.google.com/ig/api?weather=' +
                    urllib.quote(location))
             root = ElementTree.parse(urllib.urlopen(url)).getroot()
-            cond = root.find('weather/current_conditions/condition').get('data')
-            temp = root.find('weather/current_conditions/temp_f').get('data') + ' F'
-            wind = root.find('weather/current_conditions/wind_condition').get('data').replace('Wind:', 'wind')
-            humidity = ' '.join(reversed(root.find('weather/current_conditions/humidity').get('data').replace('Humidity:', 'humidity').split()))
-            place = root.find('weather/forecast_information/city').get('data')
+
+            cond_elem = root.find('weather/current_conditions/condition')
+            if cond_elem is not None:
+                cond = cond_elem.get('data')
+            else:
+                cond = None
+
+            temp_elem = root.find('weather/current_conditions/temp_f')
+            if temp_elem is not None:
+                temp = temp_elem.get('data')
+                if temp:
+                    temp += ' F'
+            else:
+                temp = None
+
+            wind_elem = root.find('weather/current_conditions/wind_condition')
+            if wind_elem is not None:
+                wind = wind_elem.get('data', '').replace('Wind:', 'wind')
+            else:
+                wind = None
+
+            humi_elem = root.find('weather/current_conditions/humidity')
+            if humi_elem is not None:
+                humidity = ' '.join(reversed(humi_elem.get('data', '').replace('Humidity:', 'humidity').split()))
+            else:
+                humidity = None
+
+            place_elem = root.find('weather/forecast_information/city')
+            if place_elem is not None:
+                place = place_elem.get('data')
+            else:
+                place = None
+
             msg = ''
             msg1 = ' and '.join(filter(None, [cond, temp]))
             msg2 = ' and '.join(filter(None, [wind, humidity]))
